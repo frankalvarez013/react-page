5b6f9c6537945b5abcd040b6e90a7a6170c37ce5
Frank Alvarez <Frankalvarez475@gmail.com>
2023-07-12 15:31:47 -0700

    add: stuff

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-  base: "/react-page",
+  root: "src",
+  base: "/react-page/",
 });
